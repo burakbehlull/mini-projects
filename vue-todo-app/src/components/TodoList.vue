fd28d0426aac3d70c571dcb2575151c7ccd1bece
31000s <burakyabgu@gmail.com>
2023-05-07 00:19:58 +0300

    feat: Added todo delete button

--- vue-todo-app/src/components/TodoList.vue
+++ vue-todo-app/src/components/TodoList.vue
@@ -4,12 +4,22 @@ import {ref} from 'vue'
 let todoName = ref('')
 let todoList = ref([])
 
+//Add Todo
 const addTodo = () =>{
     todoList.value.push({
         id: todoList.value.length + 1,
         text: todoName.value
     })
 }
+
+//Delete Todo
+
+const deleteTodo = (todoItem) => {
+  const index = todoList.value.findIndex((todo) => todo.id === todoItem.id);
+  if (index !== -1) {
+    todoList.value.splice(index, 1);
+  }
+};
 </script>
 
 <template>
@@ -19,7 +29,7 @@ const addTodo = () =>{
 <button type="submit" @click="addTodo">Ekle</button>
 
 <ul>
-    <li v-for="todo in todoList" :key="todo.id">{{ todo.text }}</li>
+    <li v-for="todo in todoList" :key="todo.id">{{ todo.text }} <button type="submit" @click="deleteTodo(todo)">x</button></li>
 </ul>
 
 </template>
