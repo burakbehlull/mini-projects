1c2f85c1c8de178504b8e38b01aac117bdae5150
burakbehlull <burakyabgu@gmail.com>
2023-12-17 00:54:59 +0300

    feat: Completion of control function

--- java-android-checking/app/src/main/java/com/example/java_android_checking/MainActivity.java
+++ java-android-checking/app/src/main/java/com/example/java_android_checking/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 	EditText editText1, editText2;
@@ -16,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
 		button = findViewById(R.id.button);
         editText1 = findViewById(R.id.editTextText);
         editText2 = findViewById(R.id.editTextText2);
+    }
+	
+	public void Kontrol(View view){
+        int sayi1, sayi2;
+        sayi1 = Integer.parseInt(editText1.getText().toString());
+        sayi2 = Integer.parseInt(editText2.getText().toString());
+
+
+        if(sayi1 > sayi2){
+            Toast.makeText(MainActivity.this, "1 sayı daha büyük", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(MainActivity.this, "sayı küçük veya eşittir", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
