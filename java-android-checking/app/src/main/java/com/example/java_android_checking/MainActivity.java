bd85060af45d5fbbe3151ab3073d94621da6b171
burakbehlull <burakyabgu@gmail.com>
2023-12-17 00:53:41 +0300

    feat: Identification of items

--- java-android-checking/app/src/main/java/com/example/java_android_checking/MainActivity.java
+++ java-android-checking/app/src/main/java/com/example/java_android_checking/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.java_android_checking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+	EditText editText1, editText2;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		button = findViewById(R.id.button);
+        editText1 = findViewById(R.id.editTextText);
+        editText2 = findViewById(R.id.editTextText2);
     }
 
 }
